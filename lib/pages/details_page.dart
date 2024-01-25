f40a794e0b174e8b540050d500ecac2acd31cb75
Aksay <aksaykanthan@gmail.com>
2024-01-25 18:52:41 +0530

    set profile

--- lib/pages/details_page.dart
+++ lib/pages/details_page.dart
@@ -1,7 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:project_01/components/button.dart';
 import 'package:project_01/components/textfield.dart';
-
 
 class UserDetails extends StatefulWidget {
   final Function()? onTap;
@@ -14,9 +15,42 @@ class UserDetails extends StatefulWidget {
 class _UserDetailsState extends State<UserDetails> {
   // controller
   final firstname = TextEditingController();
-  final lastname = TextEditingController();
+  final dob = TextEditingController();
   final confirmpassword = TextEditingController();
 
+  Future<void> addDetailsToFirestore(String collectionName, String documentId,
+      Map<String, dynamic> data) async {
+    try {
+      // Access Firestore instance
+      FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+      // Reference to the collection and document
+      CollectionReference collectionReference =
+          firestore.collection(collectionName);
+      DocumentReference documentReference = collectionReference.doc(documentId);
+
+      // Add data to the specified document
+      await documentReference.set(data);
+
+      print('Details added to Firestore successfully!');
+    } catch (e) {
+      print('Error adding details to Firestore: $e');
+    }
+  }
+
+  void addUserDetails() async {
+    String collectionName = 'users';
+
+    String customDocumentId = FirebaseAuth.instance.currentUser! as String;
+
+    Map<String, dynamic> data = {
+      'name': 'John Doe',
+      'age': 25,
+      'email': 'john.doe@example.com',
+    };
+
+    await addDetailsToFirestore(collectionName, customDocumentId, data);
+  }
 
   void getUserDetails() async {
     showDialog(
@@ -27,54 +61,42 @@ class _UserDetailsState extends State<UserDetails> {
         );
       },
     );
-
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(title: Text("Set Up Profile"),backgroundColor: Colors.grey[900],foregroundColor: Colors.white,),
       backgroundColor: Colors.grey[200],
-      body: SafeArea(
-        child: Center(
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                const SizedBox(height: 50),
-                const Text(
-                  "Set Up Profile",
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                ),
-                const SizedBox(height: 25),
-                CustomTextField(
-                  hintText: 'First Name',
-                  obscureText: false,
-                  controller: firstname,
-                ),
-                const SizedBox(height: 10),
-                CustomTextField(
-                  hintText: 'Last Name',
-                  obscureText: false,
-                  controller: lastname,
-                ),
-                const SizedBox(height: 10),
-                CustomTextField(
-                  hintText: 'Last Name',
-                  obscureText: false,
-                  controller: lastname,
-                ),
-                const SizedBox(height: 10),
-
-
-
-
-                const SizedBox(height: 25),
-                CustomButton(
-                  onTap: getUserDetails,
-                  text: "Save",
-                ),
-              ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(height: 25),
+            const SizedBox(height: 25),
+            CustomTextField(
+              hintText: 'Name',
+              obscureText: false,
+              controller: firstname,
             ),
-          ),
+            const SizedBox(height: 10),
+            CustomTextField(
+              hintText: 'DOB',
+              obscureText: false,
+              controller: dob,
+            ),
+            const SizedBox(height: 10),
+            CustomTextField(
+              hintText: 'Age',
+              obscureText: false,
+              controller: dob,
+            ),
+            const SizedBox(height: 10),
+            const SizedBox(height: 25),
+            CustomButton(
+              onTap: getUserDetails,
+              text: "Save",
+            ),
+          ],
         ),
       ),
     );
--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:project_01/pages/details_page.dart';
 
 class ProfilePage extends StatelessWidget {
   ProfilePage({super.key});
@@ -15,16 +16,31 @@ class ProfilePage extends StatelessWidget {
             "Profile",
             style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
           ),
+          actions: [
+            IconButton(
+              onPressed: () {
+                // Navigate to UserDetails page when edit icon is pressed
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => UserDetails(onTap: () {  },)),
+                );
+              },
+              icon: Icon(Icons.edit),
+            ),
+          ],
         ),
         body: ListView(
           children: [
-            SizedBox(height: 20,),
+            SizedBox(
+              height: 20,
+            ),
             CircleAvatar(
               radius: 80,
               // backgroundImage: AssetImage("assets/icons/shaun.jpeg"),
             ),
-            Text('${currUser.uid}',)
-
+            Text(
+              '${currUser.uid}',
+            )
           ],
         ));
   }
